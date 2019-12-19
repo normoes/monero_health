171db6e83f8b10ad64fc4f495ea50b38cd698c2d
Norman Moeschter <norman.moeschter@gmail.com>
2019-12-19 15:24:57 +0000

    Initial commit.

---
+++ monero_health.py
@@ -0,0 +1,102 @@
+from monerorpc.authproxy import AuthServiceProxy, JSONRPCException
+import logging
+import datetime
+import os
+import sys
+
+logging.basicConfig()
+logger = logging.getLogger("DaemonHealth")
+logger.setLevel(logging.DEBUG)
+# logging.getLogger("MoneroRPC").setLevel(logging.DEBUG)
+
+URL_DEFAULT = "127.0.0.1"
+PORT_DEFAULT = 18081
+USER_DEFAULT = ""
+PASSWD_DEFAULT = ""
+OFFSET_DEFAULT = 12
+# Possible values: https://docs.python.org/2/library/datetime.html#timedelta-objects
+OFFSET_UNIT_DEFAULT = "minutes"
+
+URL = os.environ.get("MONEROD_RPC_URL", URL_DEFAULT)
+PORT = os.environ.get("MONEROD_RPC_PORT", PORT_DEFAULT)
+USER = os.environ.get("MONEROD_RPC_USER", USER_DEFAULT)
+PASSWD = os.environ.get("MONEROD_RPC_PASSWORD", PASSWD_DEFAULT)
+OFFSET = os.environ.get("OFFSET", OFFSET_DEFAULT)
+OFFSET_UNIT = os.environ.get("OFFSET_UNIT", OFFSET_UNIT_DEFAULT)
+
+
+def is_timestamp_within_offset(timestamp=None, now=None, offset:int=OFFSET, offset_unit=OFFSET_UNIT_DEFAULT) -> bool:
+    if not timestamp or not now:
+        return None
+
+    block_offset = now - timestamp
+
+    offset = int(offset)
+
+    # Get timedelta (age) for a given block.
+    # Use default configuration in case of an error.
+    try:
+        timedelta_ = {offset_unit: offset}
+        delta = datetime.timedelta(**timedelta_)
+    except (TypeError) as e:
+        logger.warning(f"Using default offset of '{OFFSET_DEFAULT} [{OFFSET_UNIT_DEFAULT}]'. Configured wrong offset '{offset} [{offset_unit}]'. Error: '{str(e)}'.")
+        offset = OFFSET_DEFAULT
+        offset_unit = OFFSET_UNIT_DEFAULT
+        timedelta_ = {offset_unit: offset}
+        delta = datetime.timedelta(**timedelta_)
+        
+    return block_offset <= delta, offset, offset_unit
+
+
+def daemon_last_block_check(conn=None, url=URL, port=PORT, user=USER, passwd=PASSWD, offset=OFFSET, offset_unit=OFFSET_UNIT, events=None):
+    try:
+        if not conn:
+            conn = AuthServiceProxy(f"http://{user}@{url}:{port}/json_rpc", password=f"{passwd}")
+
+        logger.info(f"Checking '{url}:{port}'.")
+
+        last_block_header = conn.get_last_block_header()["block_header"]
+        last_block_timestamp = float(last_block_header["timestamp"])
+        timestamp_obj = datetime.datetime.fromtimestamp(last_block_timestamp)
+        last_block_hash = last_block_header["hash"]
+        check_timestamp = datetime.datetime.utcnow()
+        block_recent, offset, offset_unit = is_timestamp_within_offset(timestamp=timestamp_obj, now=check_timestamp, offset=offset, offset_unit=offset_unit)
+
+        if not block_recent:
+            if events:
+                event.trigger(data=f"Last block's timestamp is '{offset} [{offset_unit}]' old. Daemon: '{url}:{port}'", realm=None)
+
+        return dict({"hash": last_block_hash, "block_timestamp": timestamp_obj.isoformat(), "check_timestamp": check_timestamp.isoformat(), "block_recent": block_recent, "block_recent_offset": offset, "block_recent_offset_unit": offset_unit})
+    except (JSONRPCException) as e:
+        logger.error(f"'{str(e)}'.")
+        return None
+
+
+def daemon_status_check(conn=None, url=URL, port=PORT, user=USER, passwd=PASSWD, events=None):
+    try:
+        if not conn:
+            conn = AuthServiceProxy(f"http://{user}@{url}:{port}/json_rpc", password=f"{passwd}")
+
+        logger.info(f"Checking '{url}:{port}'.")
+
+        hard_fork_info = conn.hard_fork_info()
+        status = hard_fork_info["status"]
+        version = hard_fork_info["version"]
+
+        if not status == "OK":
+            if events:
+                event.trigger(data=f"Dameon state is '{state}'. Daemon: '{url}:{port}'", realm=None)
+
+        return dict({"status": status, "version": version})
+    except (JSONRPCException) as e:
+        logger.error(f"'{str(e)}'.")
+        return None
+
+
+def main():
+    print(daemon_last_block_check(url=URL, port=PORT, user=USER, passwd=PASSWD, offset=OFFSET, offset_unit=OFFSET_UNIT))
+    print(daemon_status_check(url=URL, port=PORT, user=USER, passwd=PASSWD))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
